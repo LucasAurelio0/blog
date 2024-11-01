d7a3f7978c03d3f7610938092cf5914bf1a23b6b
LucasAurelio0 <zeaureliosv@gmail.com>
2024-11-01 12:14:01 -0300

    feat: add main card component html/css

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
+import { MainCardComponent } from "../../components/main-card/main-card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuTitleComponent],
+  imports: [MenuTitleComponent, MainCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
