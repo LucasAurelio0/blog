4cf68b3edf980161b6dbd7e761659b620c9d7b02
LucasAurelio0 <zeaureliosv@gmail.com>
2024-11-01 18:04:55 -0300

    feat: add header component html/css

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
 import { MainCardComponent } from "../../components/main-card/main-card.component";
 import { SecondaryCardComponent } from "../../components/secondary-card/secondary-card.component";
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuTitleComponent, MainCardComponent, SecondaryCardComponent],
+  imports: [MenuTitleComponent, MainCardComponent, SecondaryCardComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
