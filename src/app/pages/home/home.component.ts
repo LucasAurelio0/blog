3f3e4fd4bd7ada40491fc3ab028d8f3709830e39
LucasAurelio0 <zeaureliosv@gmail.com>
2024-11-01 17:42:06 -0300

    feat: add secondary card component htmll/css

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
 import { MainCardComponent } from "../../components/main-card/main-card.component";
+import { SecondaryCardComponent } from "../../components/secondary-card/secondary-card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuTitleComponent, MainCardComponent],
+  imports: [MenuTitleComponent, MainCardComponent, SecondaryCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
