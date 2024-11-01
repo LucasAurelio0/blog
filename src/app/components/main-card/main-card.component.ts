dab75fad80eb35264b494a1f6f69883282bc65ed
LucasAurelio0 <zeaureliosv@gmail.com>
2024-11-01 18:53:52 -0300

    feat: added dynamic inputs for card components

--- src/app/components/main-card/main-card.component.ts
+++ src/app/components/main-card/main-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-main-card',
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './main-card.component.css'
 })
 export class MainCardComponent {
-
+  @Input()
+  photoUrl:string = 'https://archive.org/download/placeholder-image/placeholder-image.jpg'
+  @Input()
+  cardTitle:string = ''
+  @Input()
+  cardDescription:string = ''
+  @Input()
+  cardInfo:string = ''
 }
--- src/app/components/secondary-card/secondary-card.component.ts
+++ src/app/components/secondary-card/secondary-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-secondary-card',
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './secondary-card.component.css'
 })
 export class SecondaryCardComponent {
-
+  @Input()
+  photoUrl: string = 'https://archive.org/download/placeholder-image/placeholder-image.jpg'
+  @Input()
+  cardTitle: string = ''
+  @Input()
+  cardInfo: string = ''
 }
