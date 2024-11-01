a5b64b1d4dc80d17cf9ce31e930cfe5b7060bbf7
LucasAurelio0 <zeaureliosv@gmail.com>
2024-11-01 10:26:34 -0300

    feat: add initial components and global style

---
+++ src/app/components/secondary-card/secondary-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-secondary-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './secondary-card.component.html',
+  styleUrl: './secondary-card.component.css'
+})
+export class SecondaryCardComponent {
+
+}
