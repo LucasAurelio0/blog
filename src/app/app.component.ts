41823d60c27f095be84bfeec1ddadc06b1be3ebf
LucasAurelio0 <zeaureliosv@gmail.com>
2024-11-01 11:10:22 -0300

    feat: add home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { MainCardComponent } from "./components/main-card/main-card.component";
+import { SecondaryCardComponent } from "./components/secondary-card/secondary-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuTitleComponent],
+  imports: [RouterOutlet, MenuTitleComponent, HomeComponent, MainCardComponent, SecondaryCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
